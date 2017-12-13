93c518b51ccab876dc9e30cfbe7bffef84ad81aa
dedZombie <hemicar92@gmail.com>
2017-12-13 15:43:52 +0100

    Creating table

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
-import { FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database-deprecated';
+import { AngularFireList, AngularFireObject } from 'angularfire2/database/interfaces';
+// import { FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database-deprecated';
 import { Observable } from 'rxjs/Observable';
-// import { Subscriber } from 'rxjs/Subscriber';
 import { Client } from '../models/Client';
 
 @Injectable()
 export class ClientService {
 
-  clients: FirebaseListObservable<any[]>;
-  client: FirebaseObjectObservable<any>;
+  // clients: AngularFireList<any[]>;
+  clients: Observable<Client[]>;
+  client: AngularFireObject<any>;
 
   constructor(
     public af: AngularFireDatabase
   ) {
-    this.clients = this.af.list('/clients') as FirebaseListObservable<Client[]>;
+    this.clients = this.af.list('/clients').valueChanges();
+    // af.list('items').snapshotChanges().map(actions => {
+    //   return actions.map(action => ({ $key: action.key, ...action.payload.val() }));
+    // }).subscribe(items => {
+    //   return items.map(item => item.key);
+    // });
    }
 
   getClients() {
